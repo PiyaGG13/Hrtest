41f45c43209f79c8ec947f35831ebfe673063a80
PiyaGG13 <mypiyathida@gmail.com>
2026-09-19 10:54:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js - กำหนดค่า Supabase Project URL และ Anon Key
+const SUPABASE_URL = "https://naenaclvsppgwkchacrn.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_7VSIp0yFzCJyPp5rZjAFeA_njkaTKdz";
